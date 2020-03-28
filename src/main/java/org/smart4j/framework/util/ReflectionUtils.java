7206a5b2ae7929b4a90ef7567a399c924acc11f7
JaisingZ <987323161@qq.com>
2020-03-28 17:43:55 +0800

    feat(工具类): 添加反射工具类，用于实例化类、调用方法和设置属性

---
+++ src/main/java/org/smart4j/framework/util/ReflectionUtils.java
@@ -0,0 +1,59 @@
+package org.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @Author: Jaising
+ * @Description: 反射工具类
+ * @Date: Created in 3/28/2020 5:35 PM
+ */
+public final class ReflectionUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtils.class);
+
+    /**
+     * 创建实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     */
+    public static Object invokeMethod(Object obj, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置属性
+     */
+    public static void setField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
